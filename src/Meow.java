80a091d57c81efbd6b1ca0f72ecfc73dbd71aabc
LeandroLopesM <leandrolopes.tinfem2023@gmail.com>
2024-10-11 21:31:30 -0300

    mroot

--- src/Meow.java
+++ src/Meow.java
@@ -1,4 +1,8 @@
+import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Meow {
     JFrame root;
@@ -9,7 +13,12 @@ public class Meow {
     }
 
     private JFrame rootSetup() {
-        
+        JFrame that = new JFrame("A wild cat appears!");
+        that.getContentPane().add(new JLabel( new ImageIcon("src/resources/plonk.jpg") ));
+        that.setBackground(Color.black);
+        that.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        return that;
     }
 
     public static void main(String[] args) throws Exception {
