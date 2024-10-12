a86959170a66b1d5f8f31bcb15e8a6322c8cb8bd
LeandroLopesM <leandrolopes.tinfem2023@gmail.com>
2024-10-11 21:27:25 -0300

    mrow

---
+++ src/Meow.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+public class Meow {
+    JFrame root;
+    JFrame[] children;
+
+    public Meow() {
+        root = rootSetup();
+    }
+
+    private JFrame rootSetup() {
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        new Meow();
+    }
+}
